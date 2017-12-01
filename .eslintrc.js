7cfa72e240b807c62cdb3e82ba13bdaca8bbc5ba
Daniel Serratore <daniel75027@gmail.com>
2017-12-01 15:51:40 -0600

    Add ESlint

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+    "parser": "babel-eslint",
+    "env": {
+        "es6": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "sourceType": "module"
+    },
+    "rules": {
+        "indent": [
+            "error",
+            "tab"
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "double"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        "no-console":0,
+        "no-inner-declarations":0
+    }
+};
