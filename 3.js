39d256f40f5bdc0b6366e5bd5c0c1c726a21cbda
Daniel Serratore <daniel75027@gmail.com>
2017-12-04 16:03:29 -0600

    Add solution for Day 3 part 1

---
+++ 3.js
@@ -0,0 +1,25 @@
+function SpiralMemory(num){
+	/** Spiral increases by 2 'width' and 2 'height' each layer.
+	(i) is the length of one side of the spiral.
+	(c) is the number of layers.  **/
+	let i = 1;
+	let c = 1;
+
+	while (i**2 < num){
+		i += 2;
+		c += 1;
+	}
+
+	let key = (i - 1) / 2;
+	let next = (i - 1);
+	let points = [key, key + next, key + next*2, key + next*3];
+	let position = num - (i-2)**2;
+
+	// Find the closest point
+	let partial = Math.min(...points.map((point) => Math.abs(position - point)));
+	let complete = partial + (c - 1);
+
+	return complete;
+
+}
+
